c358eaef76ce4f6dbb0694d69ca29388969ab818
Damian van Dijk <davdijk12@gmail.com>
2026-06-08 12:05:18 +0200

    Remove duplicate plugin description

--- Source/WsCodeEditor/WsCodeEditorPlugin.cs
+++ Source/WsCodeEditor/WsCodeEditorPlugin.cs
@@ -1,5 +1,4 @@
 #pragma warning disable CS1591
-using System;
 using FlaxEngine;
 
 namespace WsCodeEditor
@@ -9,21 +8,5 @@ namespace WsCodeEditor
     /// </summary>
     public class WsCodeEditorPlugin : GamePlugin
     {
-        public WsCodeEditorPlugin()
-        {
-            _description = new PluginDescription
-            {
-                Name = "WsCodeEditor",
-                Category = "Scripting",
-                Author = "Wavestorm Software",
-                RepositoryUrl = null,
-                HomepageUrl = null,
-                AuthorUrl = null,
-                Description = "Embedded code editor tooling for Flax Engine projects.",
-                Version = new Version(0, 1, 0),
-                IsAlpha = true,
-                IsBeta = false,
-            };
-        }
     }
 }
